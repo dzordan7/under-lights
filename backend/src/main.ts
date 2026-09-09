08278ca173ba3596d57b267bfe3a2a2610939c1b
dzordan7 <djoleem7@gmail.com>
2026-09-09 22:43:50 +0200

    ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:4200' });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
